b03ab2676025a0337f0488d1c1c450a877ed3e82
Sumorrow <samuelgeorge78scjyz@gmail.com>
2025-04-05 23:36:08 +0800

    Add files via upload

---
+++ untitled/QQ_Client/src/qqClient/view/QQClientStart.java
@@ -0,0 +1,13 @@
+package qqClient.view;
+
+
+/**
+ *
+ * 	 客户端启动类
+ */
+public class QQClientStart {
+    public static void main(String[] args) {
+
+        new LoginFrame();
+    }
+}
---
+++ untitled/QQ_Server/src/qqServer/view/ServerStart.java
@@ -0,0 +1,12 @@
+package qqServer.view;
+
+/**
+ *
+ * 	 服务端启动类
+ */
+public class ServerStart {
+    public static void main(String[] args) {
+
+        new ServerFrame();
+    }
+}
